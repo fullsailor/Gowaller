5969bdd9097aa7a4bb84a9821a797e6c6cafe2cb
Andrew Smith <fullsailor@me.com>
2010-01-31 01:07:56 -0500

    And it works.

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,40 @@
+class ItemsController < ApplicationController
+  load_and_authorize_resource
+  
+  def index
+    @items = Item.all(:order=>'name ASC')
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+    if @item.save
+      flash[:notice] = "Successfully created item."
+      redirect_to @item
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @item.update_attributes(params[:item])
+      flash[:notice] = "Successfully updated item."
+      redirect_to @item
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @item.destroy
+    flash[:notice] = "Successfully destroyed item."
+    redirect_to items_url
+  end
+end
